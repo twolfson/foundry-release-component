019dac7db2ed987bc45cda180b5d2e7defc01775
Todd Wolfson <todd@twolfson.com>
2014-02-04 02:01:08 -0800

    Copying over tests, library, and dependencies

--- test/foundry-release-component_test.js
+++ test/foundry-release-component_test.js
@@ -1,11 +1,24 @@
-var foundry_release_component = require('../');
+// Load in dependencies
+var fs = require('fs');
+var expect = require('chai').expect;
+var componentRelease = require('../');
+var fixtureUtils = require('./utils/fixtures');
 
-describe('foundry-release-component', function () {
-  before(function () {
+// Define our test
+describe('Setting the version', function () {
+  describe('in a component module', function () {
+    var fixtureDir = fixtureUtils.fixtureDir('component');
+    before(function release (done) {
+      bowerRelease.setVersion({
+        version: '0.1.0',
+        message: 'Release 0.1.0',
+        description: null
+      }, done);
+    });
 
-  });
-
-  it('', function () {
-
+    it('updates the component.json', function () {
+      var pkgJson = fs.readFileSync(fixtureDir + '/component.json');
+      expect(JSON.parse(pkgJson)).to.have.property('version', '0.1.0');
+    });
   });
 });
